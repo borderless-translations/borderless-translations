d77512bea6018b98fa0fa77fb251ceeaf8972611
J Read <j@8bit.coffee>
2024-02-07 12:53:14 -0600

    created reducer and saga for getAllClients

--- src/redux/sagas/getAllClients.saga.js
+++ src/redux/sagas/getAllClients.saga.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
-// TODO: Rename templateDunction and replace with the new name in 'yield takeLatest()'
-function* templateFunction() {
+function* getAllClients() {
     try {
         // the config includes credentials which allow the server session to recognize the user
         const config = {
@@ -13,23 +12,19 @@ function* templateFunction() {
         // TODO: Set to correct URL and request type
         const response = yield axios.get('/api/example', config);
 
-        // TODO: Hand off the data to the correct reducer by changing the type below or delete if unneeded
-        yield put({ type: 'SET_EXAMPLE', payload: response.data });
+        // Puts the response in the reducer allClients
+        yield put({ type: 'SET_ALL_CLIENTS', payload: response.data });
 
     }
     catch (error) {
-        // TODO: Change error message
-        console.error('Example request failed', error);
+        console.error('GET for all clients failed', error);
     }
 }
 
-// Worker function  - Saga: will be fired on "EXAMPLE_ACTION" actions
-// TODO: Update saga name below. Change it in the export statement as well.
-function* ExampleSaga() {
-    // TODO: Change action below. This will be the action.type where the saga fires.
-    yield takeLatest('EXAMPLE_ACTION', templateFunction);
+// Worker function  - Saga: will be fired on "GET_ALL_CLIENTS" actions
+function* getAllClientsSaga() {
+    yield takeLatest('GET_ALL_CLIENTS', getAllClients);
 }
 
-// TODO: rename export to reflect worker function (function*) name.
-export default ExampleSaga;
+export default getAllClientsSaga;
 // TODO: Don't forget to add worker function to _root.saga.js !!!
