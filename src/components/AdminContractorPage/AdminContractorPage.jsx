eec5d5151572a16a2398e6fa29cd9e28bc6b9c2f
Chris Cantoni <chris.cantoni@gmail.com>
2024-02-07 13:04:16 -0600

    Added dispatches for contractor list.

--- src/components/AdminContractorPage/AdminContractorPage.jsx
+++ src/components/AdminContractorPage/AdminContractorPage.jsx
@@ -1,27 +1,37 @@
-import {useDispatch} from 'react-redux';
-import {useHistory} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory, Link} from 'react-router-dom';
+import { useEffect } from 'react'
 
 
 function AdminContractorPage() {
 
     const dispatch = useDispatch();
     const history = useHistory();
+    const contractorsList = useSelector(store => store.contractors);
 
-    const handleDetails = () => {
-        // Will grab details from store
-        dispatch({type: ''})
-        //history.push(`/contractor/details/${id}`)
+    const getContractors = () => {
+        dispatch({type: 'FETCH_CONTRACTORS'});
     }
+
+    const handleDetails = (id) => {
+        // Will grab details from store
+        dispatch({type: 'FETCH_CONTRACTOR_DETAILS', payload: id})
+        history.push(`/contractor/details/${id}`)
+    }
+
+useEffect(() => {
+    getContractors();
+}, []);
 
     return (
         <>
         <h1>Contractor View</h1>
-        {/* Button to add contractor, takes you to add contractor page? */}
+        {/* Button to add contractor, pops up add contractor dialog */}
         <p>Table of Contractors here</p>
         {/* Click on contractor to get taken to /details/:id for that contractor
-        
+        onClick={() => handleDetails(id)}
         Buttons for editing contractor details will be within the contractor details page 
-        
+        <Link to={`/contractor/details/${id}}><AdminContractorDetailsPage></Link>
         
         */}
         </>
