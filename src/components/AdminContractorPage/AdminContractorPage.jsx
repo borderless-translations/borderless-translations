77aefc697fd399b1e91b6f2d1bfcd7cb2c748a2e
Chris Cantoni <chris.cantoni@gmail.com>
2024-02-07 12:49:17 -0600

    AdminContractorPage features added.

--- src/components/AdminContractorPage/AdminContractorPage.jsx
+++ src/components/AdminContractorPage/AdminContractorPage.jsx
@@ -1,10 +1,29 @@
+import {useDispatch} from 'react-redux';
+import {useHistory} from 'react-router-dom';
 
 
 function AdminContractorPage() {
 
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const handleDetails = () => {
+        // Will grab details from store
+        dispatch({type: ''})
+        //history.push(`/contractor/details/${id}`)
+    }
+
     return (
         <>
         <h1>Contractor View</h1>
+        {/* Button to add contractor, takes you to add contractor page? */}
+        <p>Table of Contractors here</p>
+        {/* Click on contractor to get taken to /details/:id for that contractor
+        
+        Buttons for editing contractor details will be within the contractor details page 
+        
+        
+        */}
         </>
     );
 }
