96303894db7d14e3c2ebaa317c08de59ea7bc938
Chris Cantoni <chris.cantoni@gmail.com>
2024-02-07 21:40:32 -0600

    Grab contractor details from reducer.

--- src/components/AdminContractorDetailsPage/AdminContractorDetailsPage.jsx
+++ src/components/AdminContractorDetailsPage/AdminContractorDetailsPage.jsx
@@ -1,11 +1,14 @@
-
+import { useDispatch, useSelector } from 'react-redux';
 
 function AdminContractorDetailsPage() {
 
+    const dispatch = useDispatch();
+    const contractorDetails = useSelector(store => store.contractor);
 
     return (
         <>
             <h1>Admin Contractor Details View</h1>
+            {JSON.stringify(contractorDetails)}
         </>
     )
 }
