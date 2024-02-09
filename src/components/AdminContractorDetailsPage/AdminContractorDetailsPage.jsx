246de53b7826e9571fce5fefdfda018468d4ef8c
Chris Cantoni <chris.cantoni@gmail.com>
2024-02-09 09:58:42 -0600

    Fetch contractor details from redux store.

--- src/components/AdminContractorDetailsPage/AdminContractorDetailsPage.jsx
+++ src/components/AdminContractorDetailsPage/AdminContractorDetailsPage.jsx
@@ -1,9 +1,23 @@
 import { useDispatch, useSelector } from 'react-redux';
+import {useEffect, useState} from 'react';
+import {useHistory, useParams} from 'react-router-dom';
 
 function AdminContractorDetailsPage() {
 
     const dispatch = useDispatch();
+    const { id } = useParams();
     const contractorDetails = useSelector(store => store.contractor);
+
+    const refreshPage = () => {
+        dispatch({type: 'GET_CONTRACTOR', payload: id})
+    }
+
+
+useEffect(() => {
+    refreshPage();
+}, [])
+
+// TODO: Add editability to contractor details page
 
     return (
         <>
