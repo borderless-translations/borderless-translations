76069213f9b5c8a17c7f114d0f65f826dd059868
J Read <j@8bit.coffee>
2024-02-13 14:18:23 -0600

    cleaned up code

--- server/routes/contractor.router.js
+++ server/routes/contractor.router.js
@@ -2,10 +2,8 @@ const express = require('express');
 const {
   rejectUnauthenticated,
 } = require('../modules/authentication-middleware');
-const encryptLib = require('../modules/encryption');
-const pool = require('../modules/pool');
-const userStrategy = require('../strategies/user.strategy');
 
+const pool = require('../modules/pool');
 const router = express.Router();
 
 // Function to check if a user is an admin
@@ -113,6 +111,7 @@ router.post('/', rejectUnauthenticated, (req, res) => {
 	;
 });
 
+// TODO: Need finalized columns to create PUT
 /**
  * PUT route template
  */
@@ -127,25 +126,6 @@ router.put('/', rejectUnauthenticated, (req, res) => {
 	})
 	.catch((error) => {
 		console.error("Error in PUT", error);
-		res.sendStatus(500);
-	})
-	;
-});
-
-/**
- * DELETE route template
- */
-router.delete('/', rejectUnauthenticated, (req, res) => {
-	let querytext = `
-	// QUERY GOES HERE
-	`;
-	pool.query(querytext,[])
-	.then((result) => {
-		// Code to send goes here
-		res.sendStatus(200)
-	})
-	.catch((error) => {
-		console.error("Error in DELETE", error);
 		res.sendStatus(500);
 	})
 	;
