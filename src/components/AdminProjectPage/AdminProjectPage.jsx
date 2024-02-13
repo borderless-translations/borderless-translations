0cfacdeb8c94c9f1b9b752f8b4121c041c63ef83
Juan Balbuena <juanbalb48@gmail.com>
2024-02-13 13:22:49 -0600

    saving once again.

--- src/components/AdminProjectPage/AdminProjectPage.jsx
+++ src/components/AdminProjectPage/AdminProjectPage.jsx
@@ -15,9 +15,9 @@ function AdminProjectPage(){
     const history = useHistory();
 
     const projects = [
-        {id: 1, name: "Project 1", area_expertise: "Programming", media: "Video", service: "Closed Captions", contract_status: "Signed", from_language: "English", to_language: "Spanish"},
-        {id: 2, name: "Project 2", area_expertise: "Sports - Rugby", media: "Documentary", service: "Subtitles", contract_status: "Not Signed", from_language: "English", to_language: "Japanese"},
-        {id: 3, name: "Project 3", area_expertise: "Science - Medical", media: "Video", service: "Closed Captions", contract_status: "Signed", from_language: "English", to_language: "Korean"},
+        {id: 1, name: "Prime Academy", area_expertise: "Programming", media: "Video", service: "Closed Captions", contract_status: "Signed", from_language: "English", to_language: "Spanish"},
+        {id: 2, name: "Sky Sports", area_expertise: "Sports - Rugby", media: "Documentary", service: "Subtitles", contract_status: "Not Signed", from_language: "English", to_language: "Japanese"},
+        {id: 3, name: "Mayo Clinic", area_expertise: "Science - Medical", media: "Video", service: "Closed Captions", contract_status: "Signed", from_language: "English", to_language: "Korean"},
     ]
 
     useEffect(() => {
@@ -32,11 +32,17 @@ function AdminProjectPage(){
                 <thead>
                     <tr>
                         <td>Name</td>
+                        <br/>
                         <td>Area of Expertise</td>
+                        <br/>
                         <td>Media</td>
+                        <br/>
                         <td>Service</td>
+                        <br/>
                         <td>Contract Status</td>
+                        <br/>
                         <td>From Language</td>
+                        <br/>
                         <td>To Language</td>
                     </tr>
                 </thead>
@@ -47,12 +53,19 @@ function AdminProjectPage(){
                                 <td>
                                     <Link to={`/project/details/${project.id}`}>{project.name}</Link>
                                 </td>
+                                <br/>
                                 <td>{project.area_expertise}</td>
+                                <br/>
                                 <td>{project.media}</td>
+                                <br/>
                                 <td>{project.service}</td>
+                                <br/>
                                 <td>{project.contract_status}</td>
+                                <br/>
                                 <td>{project.from_language}</td>
+                                <br/>
                                 <td>{project.to_language}</td>
+                                <br/>
                                 <button>Edit Project</button>
                             </tr>
                         )
