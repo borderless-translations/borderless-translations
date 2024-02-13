c4b413fc81dadc260f9bed53c1b71e67856ba48c
J Read <j@8bit.coffee>
2024-02-13 15:37:32 -0600

    added middleware and created router

---
+++ server/routes/client.router.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const {
+  rejectUnauthenticated, requireAdmin
+} = require('../modules/authentication-middleware');
+const encryptLib = require('../modules/encryption');
+const pool = require('../modules/pool');
+const userStrategy = require('../strategies/user.strategy');
+
+const router = express.Router();
+
+/**
+ * GET route template
+ */
+router.get('/', rejectUnauthenticated, (req, res) => {
+	let querytext = `
+	    // QUERY GOES HERE
+	`;
+	pool.query(querytext,[])
+        .then((result) => {
+            // Code to send goes here
+        })
+        .catch((error) => {
+            console.error("Error in GET", error);
+            res.sendStatus(500);
+        })
+	;
+});
+
+/**
+ * POST route template
+ */
+router.post('/', rejectUnauthenticated, (req, res) => {
+	let querytext = `
+	    // QUERY GOES HERE
+	`;
+	pool.query(querytext,[])
+        .then((result) => {
+            // Code to send goes here
+            res.sendStatus(201)
+        })
+        .catch((error) => {
+            console.error("Error in POST", error);
+            res.sendStatus(500);
+        })
+	;
+});
+
+/**
+ * PUT route template
+ */
+router.put('/', rejectUnauthenticated, (req, res) => {
+	let querytext = `
+	    // QUERY GOES HERE
+	`;
+	pool.query(querytext,[])
+        .then((result) => {
+            // Code to send goes here
+            res.sendStatus(200)
+        })
+        .catch((error) => {
+            console.error("Error in PUT", error);
+            res.sendStatus(500);
+        })
+	;
+});
+
+/**
+ * DELETE route template
+ */
+router.delete('/', rejectUnauthenticated, (req, res) => {
+	let querytext = `
+	    // QUERY GOES HERE
+	`;
+	pool.query(querytext,[])
+        .then((result) => {
+            // Code to send goes here
+            res.sendStatus(200)
+        })
+        .catch((error) => {
+            console.error("Error in DELETE", error);
+            res.sendStatus(500);
+        })
+	;
+});
+
+module.exports = router;
