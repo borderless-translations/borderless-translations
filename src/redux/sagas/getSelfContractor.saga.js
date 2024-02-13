84ca4f601503d0acd1fe864a912d5ef089b1ac08
J Read <j@8bit.coffee>
2024-02-13 14:03:14 -0600

    added saga for calling self contractor info

---
+++ src/redux/sagas/getSelfContractor.saga.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+function* getContractorSelf() {
+    try {
+        // the config includes credentials which allow the server session to recognize the user
+        const config = {
+            headers: { 'Content-Type': 'application/json' },
+            withCredentials: true,
+        };
+
+        // TODO: Set to correct URL and request type
+        const response = yield axios.get(`/api/contractor/self`, config);
+
+        yield put({ type: 'SET_CONTRACTOR', payload: response.data[0] });
+
+    }
+    catch (error) {
+        console.error('GET for contractor self has failed.', error);
+    }
+}
+
+// Worker function  - Saga: will be fired on "GET_CONTRACTOR" actions
+function* getContractorSelfSaga() {
+    yield takeLatest('GET_CONTRACTOR_SELF', getContractorSelf);
+}
+
+export default getContractorSelfSaga;
