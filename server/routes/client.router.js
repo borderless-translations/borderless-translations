66f09027807eb20f59d744ea989e59f58065d0c5
J Read <j@8bit.coffee>
2024-02-14 10:18:38 -0600

    added GET routes

--- server/routes/client.router.js
+++ server/routes/client.router.js
@@ -2,22 +2,17 @@ const express = require('express');
 const {
   rejectUnauthenticated, requireAdmin
 } = require('../modules/authentication-middleware');
-const encryptLib = require('../modules/encryption');
 const pool = require('../modules/pool');
-const userStrategy = require('../strategies/user.strategy');
-
 const router = express.Router();
 
-/**
- * GET route template
- */
-router.get('/', rejectUnauthenticated, (req, res) => {
+// GET all clients. Admin required
+router.get('/', requireAdmin, (req, res) => {
 	let querytext = `
-	    // QUERY GOES HERE
+	    SELECT * FROM "clients";
 	`;
-	pool.query(querytext,[])
+	pool.query(querytext)
         .then((result) => {
-            // Code to send goes here
+            res.send(result.rows);
         })
         .catch((error) => {
             console.error("Error in GET", error);
@@ -26,6 +21,24 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 	;
 });
 
+// GET specific client. Admin required
+router.get('/:id', requireAdmin, (req, res) => {
+	let querytext = `
+	    SELECT * FROM "clients"
+        WHERE "clients"."id" = $1;
+	`;
+	pool.query(querytext,[req.params.id])
+        .then((result) => {
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.error("Error in GET", error);
+            res.sendStatus(500);
+        })
+	;
+});
+
+// TODO: Need finalized columns for POST
 /**
  * POST route template
  */
@@ -45,6 +58,7 @@ router.post('/', rejectUnauthenticated, (req, res) => {
 	;
 });
 
+// TODO: Need finalized columns for PUT
 /**
  * PUT route template
  */
