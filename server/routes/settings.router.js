d0d34be2bc812f1cd55e12acc3431b57cded083d
J Read <j@8bit.coffee>
2024-02-14 12:13:10 -0600

    Added routes for contractor_languages

--- server/routes/settings.router.js
+++ server/routes/settings.router.js
@@ -164,4 +164,94 @@ router.delete('/rate/:id', requireAdmin, (req, res) => {
 	;
 });
 
+// GET all contractor_languages
+router.get('/contractor/language', requireAdmin, (req, res) => {
+	let querytext = `
+		SELECT * FROM "contractor_language";
+	`;
+	pool.query(querytext,[])
+		.then((result) => {
+			res.send(result.rows);
+		})
+		.catch((error) => {
+			console.error("Error in GET", error);
+			res.sendStatus(500);
+		})
+	;
+});
+
+// GET all specific contractor_languages
+router.get('/:id/language', requireAdmin, (req, res) => {
+	let querytext = `
+		SELECT * FROM "contractor_language"
+        WHERE "user_id" = $1;
+	`;
+	pool.query(querytext,[req.params.id])
+		.then((result) => {
+			res.send(result.rows);
+		})
+		.catch((error) => {
+			console.error("Error in GET", error);
+			res.sendStatus(500);
+		})
+	;
+});
+
+// GET contractor_languages for self
+router.get('/:id/language', rejectUnauthenticated, (req, res) => {
+	let querytext = `
+		SELECT * FROM "contractor_language"
+        WHERE "user_id" = $1;
+	`;
+	pool.query(querytext,[req.user.id])
+		.then((result) => {
+			res.send(result.rows);
+		})
+		.catch((error) => {
+			console.error("Error in GET", error);
+			res.sendStatus(500);
+		})
+	;
+});
+
+// POST new language
+router.post('/contractor/language', requireAdmin, (req, res) => {
+	let querytext = `
+		INSERT INTO "contractor_language" ("user_id", "from_language_id", "to_language_id")
+        VALUES ($1, $2, $3);
+	`;
+	pool.query(querytext,[req.user.id, req.body.from_language_id, req.body.to_language_id])
+		.then((result) => {
+			// Code to send goes here
+			res.sendStatus(201)
+		})
+		.catch((error) => {
+			console.error("Error in POST new language", error);
+			res.sendStatus(500);
+		})
+	;
+});
+
+// DELETE language from table
+router.delete('/contractor/language/:id', rejectUnauthenticated, (req, res) => {
+    let querytext = `
+		DELETE FROM "contractor_language"
+        WHERE 
+            "id" = $1
+            AND "user_id" = $2;
+	`;
+	pool.query(querytext,[req.params.id, req.user.id])
+		.then((result) => {
+			// Code to send goes here
+			res.sendStatus(200)
+		})
+		.catch((error) => {
+			console.error("Error in DELETE", error);
+			res.sendStatus(500);
+		})
+	;
+});
+
+
+
 module.exports = router;
