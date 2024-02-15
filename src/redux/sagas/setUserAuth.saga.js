d81a6167a74142331b6f3be17251791aea2b58f9
J Read <j@8bit.coffee>
2024-02-15 10:49:13 -0600

    Created setUserAuth.saga and created corresponding route in user.router

---
+++ src/redux/sagas/setUserAuth.saga.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+// TODO: Rename templateFunction and replace with the new name in 'yield takeLatest()'
+function* setUserAuth(action) {
+    try {
+        // the config includes credentials which allow the server session to recognize the user
+        const config = {
+            headers: { 'Content-Type': 'application/json' },
+            withCredentials: true,
+        };
+
+        yield axios.put('/api/user/auth', action.payload, config);
+        console.log(`User id: ${action.payload.id} set to auth level: ${action.payload.type}.`);
+    }
+    catch (error) {
+        console.error('Error in PUT auth level', error);
+    }
+}
+
+// Worker function  - Saga: will be fired on "SET_USER_AUTH" actions
+function* setUserAuthSaga() {
+    yield takeLatest('SET_USER_AUTH', setUserAuth);
+}
+
+export default setUserAuthSaga;
