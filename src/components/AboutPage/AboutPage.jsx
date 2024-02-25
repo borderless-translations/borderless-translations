dbf4d4061393bfe6d528ea875c941ab6fcd172f9
Chris Cantoni <chris.cantoni@gmail.com>
2024-02-25 10:25:43 -0600

    About page design.

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Card from '@mui/material/Card';
+import '../InfoPage/InfoPage.css';
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -8,13 +10,15 @@ import React from 'react';
 function AboutPage() {
   return (
     <div className="container">
-      <div>
+      <div className="aboutPage">
+        <Card sx={{maxWidth: 600, margin: 'auto', backgroundColor: '#48a6cd', color: 'white', padding: 5}}>
         <h3>Borderless Translations About Page</h3>
         <p>Welcome to the Borderless Translations contractor management site. This site
-          will allow you to keep track of your projects and log their progress in order to coordinate
-          with Borderless Translations as well as your fellow translators and proofreaders.
+          allows contractors to keep track of their projects and log their progress in order to coordinate assignments
+          with Borderless Translations as well as their fellow translators and proofreaders.
         </p>
         <p>If you haven't yet, <a href="/#/home">login</a> now to fill out your profile and join the BT team!</p>
+        </Card>
       </div>
     </div>
   );
