30d20b9849cdcd3edeb3c8709b8a7ba91bff1711
Chris Cantoni <chris.cantoni@gmail.com>
2024-02-26 17:27:07 -0600

    Formulated Project Details into a table to match rest of app.

--- src/components/AdminProjectDetails/AdminProjectDetails.jsx
+++ src/components/AdminProjectDetails/AdminProjectDetails.jsx
@@ -4,6 +4,8 @@ import { useParams, useHistory, Link } from 'react-router-dom';
 import { Select, MenuItem, Stack, Tooltip, IconButton, Button, TextField, InputAdornment, Box } from '@mui/material';
 import AdminProjectModal from '../AdminProjectModal/AdminProjectModal';
 import { DateTime } from 'luxon';
+import {TableContainer, Table, TableCell, TableBody, TableHead, TableRow} from '@mui/material';
+import Paper from '@mui/material/Paper';
 
 
 function AdminProjectDetails(){
@@ -32,19 +34,38 @@ function AdminProjectDetails(){
     return(
         <div className="container"> 
             <h2 style={{ margin: '20px 50px'}}>Admin Project Details</h2>
-            {JSON.stringify(project)}
+
             <Stack direction='row' sx={{ margin: '0px 100px', justifyContent: 'center' }}></Stack>
-            <p>Client Name: {project.client_name}</p>
-            <p>Description: {project.description}</p>
-            <p>Due By: {DateTime.fromISO(project.due_at).toFormat('DDD')}</p>
-            <p>Status: {project.status}</p>
-            <p>Translator Status: {project.translator_status}</p>
-            <p>Proofreader Status: {project.proofreader_status}</p>
-            <p>From Language: {project.from_language_name}</p>
-            <p>To Language: {project.to_language_name}</p>
-            <p>Service Notes: {project.service}</p>
-            <p>File Link: <a href={project.file_link} target="_blank">{project.file_link}</a></p>
-            
+            <TableContainer component={Paper}>
+             <Table sx={{ minWidth: 650 }} aria-label="simple table" className="adminContractorDetailsTable">
+                <TableHead>
+                    <TableRow sx={{"& th": {color: "white", fontWeight: 700, backgroundColor: "#332c7b"}}}>
+                        <TableCell align="center">Client Name</TableCell>
+                        <TableCell align="center">Description</TableCell>
+                        <TableCell align="center">Due By</TableCell>
+                        <TableCell align="center">Status</TableCell>
+                        <TableCell align="center">Translator Status</TableCell>
+                        <TableCell align="center">Proofreader Status</TableCell>
+                        <TableCell align="center">From Language</TableCell>
+                        <TableCell align="center">To Language</TableCell>
+                        <TableCell align="center">Service Notes</TableCell>
+                        <TableCell align="center">File Link</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                        <TableCell align="center">{project.client_name}</TableCell>
+                        <TableCell align="center">{project.description}</TableCell>
+                        <TableCell align="center">{DateTime.fromISO(project.due_at).toFormat('DDD')}</TableCell>
+                        <TableCell align="center">{project.status}</TableCell>
+                        <TableCell align="center">{project.translator_name}<br/>{project.translator_status}</TableCell>
+                        <TableCell align="center">{project.proofreader_name}<br/>{project.proofreader_status}</TableCell>
+                        <TableCell align="center">{project.from_language_name}</TableCell>
+                        <TableCell align="center">{project.to_language_name}</TableCell>
+                        <TableCell align="center">{project.service}</TableCell>
+                        <TableCell align="center" sx={{width: '5%'}}><a href={project.file_link} target="_blank">{project.file_link}</a></TableCell>
+                </TableBody>
+              </Table>
+            </TableContainer>
         
 
         {modalOpen && <AdminProjectModal closeModal={() => { setModalOpen(false), setProjectToEdit(null) }} defaultValues={project} />}
